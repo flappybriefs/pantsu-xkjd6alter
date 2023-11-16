2286609611074abca0d63d48e1d8867693f5f63d
胖次 <ly0251@outlook.com>
2023-11-17 01:14:02 +0800

    更改文件名称，使之统一

--- rime.lua
+++ rime.lua
@@ -36,7 +36,7 @@ single_char_filter = require("single_char")
 --- 修改自 @懒散 TsFreddie https://github.com/TsFreddie/jdc_lambda/blob/master/rime/lua/xkjdc_sbb_hint.lua
 -- 可由 schema 的 danzi_mode 与 wxw_hint 开关控制
 -- 详见 `lua/xkjd6_filter.lua`
-xkjd6_filter = require("xkjd6_filter")
+pantsu_filter = require("pantsu_filter")
 
 -- 顶功处理器
 topup_processor = require("for_topup")
@@ -50,6 +50,52 @@ number_translator = require("xnumber")
 
 -- 用 ' 作为次选键
 smart_2 = require("smart_2")
+
+-- easy_en_enhance_filter: 连续输入增强
+-- 详见 `lua/easy_en.lua`
+local easy_en = require("easy_en")
+easy_en_enhance_filter = easy_en.enhance_filter
+
+
+
+
+local english = require("english")()
+english_processor = english.processor
+english_segmentor = english.segmentor
+english_translator = english.translator
+english_filter = english.filter
+english_filter0 = english.filter0
+
+--[[
+librime-lua 样例
+```
+  engine:
+    translators:
+      - lua_translator@lua_function3
+      - lua_translator@lua_function4
+    filters:
+      - lua_filter@lua_function1
+      - lua_filter@lua_function2
+```
+其中各 `lua_function` 为在本文件所定义变量名。
+--]]
+
+--[[
+本文件的后面是若干个例子，按照由简单到复杂的顺序示例了 librime-lua 的用法。
+每个例子都被组织在 `lua` 目录下的单独文件中，打开对应文件可看到实现和注解。
+
+各例可使用 `require` 引入。
+```
+  foo = require("bar")
+```
+可认为是载入 `lua/bar.lua` 中的例子，并起名为 `foo`。
+配方文件中的引用方法为：`...@foo`。
+--]]
+
+
+
+
+
 
 
 
