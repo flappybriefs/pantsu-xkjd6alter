654eb729b22fb66c901c4f6ec8de4f96fcbe757c
胖次 <ly0251@outlook.com>
2023-12-16 15:26:41 +0800

    日常优化

--- lua/pantsu_filter.lua
+++ lua/pantsu_filter.lua
@@ -2,14 +2,21 @@ local function startswith(str, start)
     return string.sub(str, 1, string.len(start)) == start
 end
 
-local function hint(cand, context, reverse)
+local function hint(cand, env)
     if utf8.len(cand.text) < 2 then
         return false
     end
+    local context = env.engine.context
+    local reverse = env.reverse
+    local s = env.s
+    local b = env.b
     
     local lookup = " " .. reverse:lookup(cand.text) .. " "
-    local short = string.match(lookup, " ([auiovbcdefghjklmnpqrstwxyz][auiov]+) ") or 
-                  string.match(lookup, " ([bcdefghjklmnpqrstwxyz][bcdefghjklmnpqrstwxyz]) ")
+    local short = string.match(lookup, " (["..s.."]["..b.."]+) ") or 
+                  string.match(lookup, " (["..s.."]["..s.."]) ") or
+                  string.match(lookup, " (["..s.."]["..s.."]["..b.."]) ") or
+                  string.match(lookup, " (["..b.."]["..b.."]) ") or
+                  string.match(lookup, " (["..b.."]["..b.."]["..b.."]) ")
     local input = context.input 
     if short and utf8.len(input) > utf8.len(short) and not startswith(short, input) then
         -- cand:get_genuine().comment = cand.comment .. "〔" .. short .. "〕"
@@ -32,13 +39,15 @@ local function commit_hint(cand, hint_text)
     -- cand:get_genuine().comment = cand.comment
 end
 
+
+
 local function filter(input, env)
     local is_danzi = env.engine.context:get_option('danzi_mode')
     local is_on = env.engine.context:get_option('sbb_hint')
     local hint_text = env.engine.schema.config:get_string('hint_text') or '⚠️'
     local first = true
     local input_text = env.engine.context.input
-    local no_commit = (input_text:len() < 4 and input_text:match("^[bcdefghjklmnpqrstwxyz]+$")) or (input_text:match("^[avuio]+$"))
+    local no_commit = (input_text:len() < 4 and input_text:match("^["..env.s.."]+$")) or (input_text:match("^["..env.b.."]+$"))
     for cand in input:iter() do
         -- if first and no_commit and cand.type ~= 'completion' then
         if first and no_commit then
@@ -48,15 +57,21 @@ local function filter(input, env)
         first = false
         if not is_danzi or danzi(cand) then
             if is_on then
-            hint(cand, env.engine.context, env.reverse)
+            hint(cand, env)
             end
             yield(cand)
         end
+        
     end
 end
 
 local function init(env)
-    env.reverse = ReverseDb("build/pantsu.extended.reverse.bin")
+    local config = env.engine.schema.config
+    local dict_name = config:get_string("translator/dictionary")
+
+    env.b = config:get_string("topup/topup_with")
+    env.s = config:get_string("topup/topup_this")
+    env.reverse = ReverseDb("build/".. dict_name .. ".reverse.bin")
 end
 
 return { init = init, func = filter }
